55be135613edc0384ab256759fd01b34b92db246
Gary Allen <g.allen@hud.ac.uk>
2019-07-18 14:13:16 +0100

    Added a few transition effects just for fun.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Dr Gary Allen, The University of Huddersfield.
 
 This project was created in IntelliJ using Scene Builder to create the user interfaces.
 
-The "simpleDemo" is very basic.  The main class loads a very simple GUI.  Clicking the buttons either changes the Scene on the "primaryStage", or creates a pop-up window in a new Stage.
+The "simpleDemo" is very basic.  The main class loads a very simple GUI.  Clicking the buttons either changes the Scene on the "primaryStage", or creates a pop-up window in a new Stage.  To make this a bit more interesting scene changes within the stage involve a randomly selected 'Transition', either rotation, fading, or scaling.  Note that you cannot add these transitions to stages, only to scenes and nodes.
 
 The "complexDemo" is more sophisticated.  The 'top level' GUI can create an input window and an output window.  When the input window is created the main GUI controller passes a reference to itself to the input window controller, so that the input window can then call a method of the parent GUI.  When the output window is created the main GUI controller passes data to the output window controller for display.  The demo therefore shows data being passed "up" from a pop-up to the main GUI, and data being passed "down" from the main GUI to a pop-up.
 
--- src/simpleDemo/MainController.java
+++ src/simpleDemo/MainController.java
@@ -1,6 +1,9 @@
 package simpleDemo;
 
 import complexDemo.Employee;
+import javafx.animation.FadeTransition;
+import javafx.animation.RotateTransition;
+import javafx.animation.ScaleTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,8 +12,10 @@ import javafx.scene.control.Button;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.Window;
+import javafx.util.Duration;
 
 import java.io.IOException;
+import java.util.Random;
 
 public class MainController {
 
@@ -20,6 +25,43 @@ public class MainController {
     public void changeScene(ActionEvent actionEvent) throws IOException {
         Window mainWindow = changeSceneButton.getScene().getWindow();
         Parent newRoot = FXMLLoader.load(getClass().getResource("ChangedScene.fxml"));
+
+        // Let's have a bit of fun here with some transition effects as the new scene is displayed
+        // Pick a random number, 1 to 3:
+        int rand = new Random().nextInt(3) + 1;
+
+        // Use the random number to pick a random transition
+        switch (rand) {
+            case 1: // Rotation
+                RotateTransition rot = new RotateTransition();
+                rot.setDuration(Duration.millis(500));
+                rot.setFromAngle(0.0);
+                rot.setToAngle(360.0);
+                rot.setNode(newRoot);
+                rot.play();
+                break;
+
+            case 2: // Fade
+                FadeTransition fader = new FadeTransition();
+                fader.setDuration(Duration.millis(2000));
+                fader.setFromValue(0.1);
+                fader.setToValue(1.0);
+                fader.setNode(newRoot);
+                fader.play();
+                break;
+
+            case 3: // Scale
+                ScaleTransition scaler = new ScaleTransition();
+                scaler.setDuration(Duration.millis(500));
+                scaler.setFromX(0.1);
+                scaler.setToX(1.0);
+                scaler.setFromY(0.1);
+                scaler.setToY(1.0);
+                scaler.setNode(newRoot);
+                scaler.play();
+                break;
+        }
+
         mainWindow.getScene().setRoot(newRoot);
     }
 
